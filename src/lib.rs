c5d77ce71a38cf58a395c9d4608b899ee8789ad9
Joe Polny <joepolny@gmail.com>
2025-04-07 14:18:32 -0400

    use types instead of structs

--- src/lib.rs
+++ src/lib.rs
@@ -23,126 +23,113 @@ pub enum Error {
     ConvertFail(i32),
 }
 
-#[derive(Clone, Debug)]
-pub struct PublicKey([u8; PUBLIC_KEY_SIZE]);
+pub type PublicKey = [u8; PUBLIC_KEY_SIZE];
+pub type PrivateKey = [u8; PRIVATE_KEY_SIZE];
+pub type CompressedSignature = Vec<u8>;
+pub type CTSignature = [u8; CT_SIGNATURE_SIZE];
 
-#[derive(Clone, Debug)]
-pub struct PrivateKey([u8; PRIVATE_KEY_SIZE]);
-
-#[derive(Clone, Debug)]
-pub struct CompressedSignature(Vec<u8>);
-
-#[derive(Clone, Debug)]
-pub struct CTSignature([u8; CT_SIGNATURE_SIZE]);
-
-impl PublicKey {
-    pub fn verify(&self, signature: &CompressedSignature, msg: &[u8]) -> Result<(), Error> {
-        let result = unsafe {
-            if msg.is_empty() {
-                falcon_det1024_verify_compressed(
-                    signature.0.as_ptr() as *const _,
-                    signature.0.len(),
-                    self.0.as_ptr() as *const _,
-                    ptr::null(),
-                    0,
-                )
-            } else {
-                falcon_det1024_verify_compressed(
-                    signature.0.as_ptr() as *const _,
-                    signature.0.len(),
-                    self.0.as_ptr() as *const _,
-                    msg.as_ptr() as *const _,
-                    msg.len(),
-                )
-            }
-        };
-
-        if result != 0 {
-            return Err(Error::VerifyFail(result));
-        }
-
-        Ok(())
-    }
-
-    pub fn verify_ct(&self, signature: &CTSignature, msg: &[u8]) -> Result<(), Error> {
-        let result = unsafe {
-            if msg.is_empty() {
-                falcon_det1024_verify_ct(
-                    signature.0.as_ptr() as *const _,
-                    self.0.as_ptr() as *const _,
-                    ptr::null(),
-                    0,
-                )
-            } else {
-                falcon_det1024_verify_ct(
-                    signature.0.as_ptr() as *const _,
-                    self.0.as_ptr() as *const _,
-                    msg.as_ptr() as *const _,
-                    msg.len(),
-                )
-            }
-        };
-
-        if result != 0 {
-            return Err(Error::VerifyFail(result));
-        }
-
-        Ok(())
-    }
-}
-
-impl PrivateKey {
-    pub fn sign_compressed(&self, msg: &[u8]) -> Result<CompressedSignature, Error> {
-        let mut sig = vec![0u8; SIGNATURE_MAX_SIZE];
-        let mut sig_len = 0;
-
-        let result = unsafe {
-            if msg.is_empty() {
-                falcon_det1024_sign_compressed(
-                    sig.as_mut_ptr() as *mut _,
-                    &mut sig_len,
-                    self.0.as_ptr() as *const _,
-                    ptr::null(),
-                    0,
-                )
-            } else {
-                falcon_det1024_sign_compressed(
-                    sig.as_mut_ptr() as *mut _,
-                    &mut sig_len,
-                    self.0.as_ptr() as *const _,
-                    msg.as_ptr() as *const _,
-                    msg.len(),
-                )
-            }
-        };
-
-        if result != 0 {
-            return Err(Error::SignFail(result));
-        }
-
-        sig.truncate(sig_len);
-        Ok(CompressedSignature(sig))
-    }
-}
-
-impl CompressedSignature {
-    pub fn convert_to_ct(&self) -> Result<CTSignature, Error> {
-        let mut sig_ct = CTSignature([0u8; CT_SIGNATURE_SIZE]);
-
-        let result = unsafe {
-            falcon_det1024_convert_compressed_to_ct(
-                sig_ct.0.as_mut_ptr() as *mut _,
-                self.0.as_ptr() as *const _,
-                self.0.len(),
+pub fn verify(public_key: &PublicKey, signature: &CompressedSignature, msg: &[u8]) -> Result<(), Error> {
+    let result = unsafe {
+        if msg.is_empty() {
+            falcon_det1024_verify_compressed(
+                signature.as_ptr() as *const _,
+                signature.len(),
+                public_key.as_ptr() as *const _,
+                ptr::null(),
+                0,
             )
-        };
-
-        if result != 0 {
-            return Err(Error::ConvertFail(result));
+        } else {
+            falcon_det1024_verify_compressed(
+                signature.as_ptr() as *const _,
+                signature.len(),
+                public_key.as_ptr() as *const _,
+                msg.as_ptr() as *const _,
+                msg.len(),
+            )
         }
+    };
 
-        Ok(sig_ct)
+    if result != 0 {
+        return Err(Error::VerifyFail(result));
     }
+
+    Ok(())
+}
+
+pub fn verify_ct(public_key: &PublicKey, signature: &CTSignature, msg: &[u8]) -> Result<(), Error> {
+    let result = unsafe {
+        if msg.is_empty() {
+            falcon_det1024_verify_ct(
+                signature.as_ptr() as *const _,
+                public_key.as_ptr() as *const _,
+                ptr::null(),
+                0,
+            )
+        } else {
+            falcon_det1024_verify_ct(
+                signature.as_ptr() as *const _,
+                public_key.as_ptr() as *const _,
+                msg.as_ptr() as *const _,
+                msg.len(),
+            )
+        }
+    };
+
+    if result != 0 {
+        return Err(Error::VerifyFail(result));
+    }
+
+    Ok(())
+}
+
+pub fn sign_compressed(private_key: &PrivateKey, msg: &[u8]) -> Result<CompressedSignature, Error> {
+    let mut sig = vec![0u8; SIGNATURE_MAX_SIZE];
+    let mut sig_len = 0;
+
+    let result = unsafe {
+        if msg.is_empty() {
+            falcon_det1024_sign_compressed(
+                sig.as_mut_ptr() as *mut _,
+                &mut sig_len,
+                private_key.as_ptr() as *const _,
+                ptr::null(),
+                0,
+            )
+        } else {
+            falcon_det1024_sign_compressed(
+                sig.as_mut_ptr() as *mut _,
+                &mut sig_len,
+                private_key.as_ptr() as *const _,
+                msg.as_ptr() as *const _,
+                msg.len(),
+            )
+        }
+    };
+
+    if result != 0 {
+        return Err(Error::SignFail(result));
+    }
+
+    sig.truncate(sig_len);
+    Ok(sig)
+}
+
+pub fn convert_to_ct(signature: &CompressedSignature) -> Result<CTSignature, Error> {
+    let mut sig_ct = [0u8; CT_SIGNATURE_SIZE];
+
+    let result = unsafe {
+        falcon_det1024_convert_compressed_to_ct(
+            sig_ct.as_mut_ptr() as *mut _,
+            signature.as_ptr() as *const _,
+            signature.len(),
+        )
+    };
+
+    if result != 0 {
+        return Err(Error::ConvertFail(result));
+    }
+
+    Ok(sig_ct)
 }
 
 pub fn generate_key(seed: &[u8]) -> Result<(PublicKey, PrivateKey), Error> {
@@ -156,14 +143,14 @@ pub fn generate_key(seed: &[u8]) -> Result<(PublicKey, PrivateKey), Error> {
         }
     }
 
-    let mut public_key = PublicKey([0u8; PUBLIC_KEY_SIZE]);
-    let mut private_key = PrivateKey([0u8; PRIVATE_KEY_SIZE]);
+    let mut public_key = [0u8; PUBLIC_KEY_SIZE];
+    let mut private_key = [0u8; PRIVATE_KEY_SIZE];
 
     let result = unsafe {
         falcon_det1024_keygen(
             &mut rng,
-            private_key.0.as_mut_ptr() as *mut _,
-            public_key.0.as_mut_ptr() as *mut _,
+            private_key.as_mut_ptr() as *mut _,
+            public_key.as_mut_ptr() as *mut _,
         )
     };
 
@@ -184,11 +171,11 @@ mod tests {
         let (pk, sk) = generate_key(seed).unwrap();
         
         let msg = b"test message";
-        let sig = sk.sign_compressed(msg).unwrap();
+        let sig = sign_compressed(&sk, msg).unwrap();
         
-        pk.verify(&sig, msg).unwrap();
+        verify(&pk, &sig, msg).unwrap();
         
-        let sig_ct = sig.convert_to_ct().unwrap();
-        pk.verify_ct(&sig_ct, msg).unwrap();
+        let sig_ct = convert_to_ct(&sig).unwrap();
+        verify_ct(&pk, &sig_ct, msg).unwrap();
     }
 }
