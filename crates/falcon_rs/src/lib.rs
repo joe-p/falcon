c36a0c0736e02411b54901097ab22cb3db041d02
Joe Polny <joepolny@gmail.com>
2025-12-04 11:35:59 -0500

    remove warnings in falcon_sys

--- crates/falcon_rs/src/lib.rs
+++ crates/falcon_rs/src/lib.rs
@@ -1,8 +1,8 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-
 mod falcon_sys {
+    #![allow(dead_code)]
+    #![allow(non_upper_case_globals)]
+    #![allow(non_camel_case_types)]
+    #![allow(non_snake_case)]
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
 
